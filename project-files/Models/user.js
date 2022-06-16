aec7f76cdfd58d967639ba12919c1cc2eeebb6eb
A1I21 <87023979+A1I21@users.noreply.github.com>
2022-06-16 11:12:36 +0300

    schemma done

--- project-files/Models/user.js
+++ project-files/Models/user.js
@@ -1,6 +1,69 @@
-const mongoose = require('mongoose')
-const userSchema=mongoose.Schema({
-    //your schema
-})
-
-module.exports = mongoose.model('user',userSchema)
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  info: {
+    type: [String],
+  },
+  Q: {
+    type: [ObjectId],
+    ref: "questions",
+  },
+  A: {
+    type: [ObjectId],
+    ref: "answers",
+  },
+});
+const quastionSchema = mongoose.Schema({
+  Thequestion: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  level: {
+    type: String,
+  },
+  tags: {
+    type: [String],
+  },
+  scoore: {
+    type: Number,
+    requrid: true,
+  },
+  author: {
+    type: ObjectId,
+    ref: "user",
+  },
+  numUsersSolved: {
+    type: [ObjectId],
+    ref: "User",
+  },
+});
+const answarSchema = mongoose.Schema({
+  question: {
+    type: ObjectId,
+    ref: "questions",
+  },
+  user: {
+    type: ObjectId,
+    ref: "user",
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("questions", quastionSchema);
+module.exports = mongoose.model("answers", answarSchema);
--- project-files/Routes/index.js
+++ project-files/Routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 
-// your routes here
+
 
 module.exports = router
